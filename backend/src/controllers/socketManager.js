36cab7dc5409d76fffc66abc78e3c593d12f8bfd
BatmanVigilante <ashishmohan625@gmail.com>
2025-03-20 23:38:45 +0530

    Socket.io jargon

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -2,5 +2,12 @@ import { Server } from "socket.io";  // ✅ Import the Server class
 
 export const connectToSocket = (server) => {
     const io = new Server(server);
+
+    io.on("connection", (socket) => {
+        socket.on("message", (message) => {
+            console.log(message);
+        });   
+    });
+
     return io;
 };
